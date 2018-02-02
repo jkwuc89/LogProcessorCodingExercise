d0dcd8e16833a8e0df213bd466e497a1f9b1f600
Keith Wedinger <keith.wedinger@improving.com>
2018-02-02 10:47:23 -0500

    Completed all exercise requirements

--- src/main/kotlin/com/keithwedinger/LogProcessor.kt
+++ src/main/kotlin/com/keithwedinger/LogProcessor.kt
@@ -5,6 +5,9 @@ import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.collections.HashMap
 import kotlin.system.exitProcess
+import java.util.stream.Collectors
+
+
 
 /**
  * LogProcessor Coding Exercise
@@ -26,9 +29,35 @@ fun main(args: Array<String>) {
             val logProcessor = LogProcessor()
             logProcessor.parseLogEntries(logEntries)
 
-            // Report on the parsing results
-            val (mostFrequentlyRequestedUrl, count) = logProcessor.getMostFrequentlyRequestedUrl()
-            println("URL that was most frequently requested:\nURL: ${mostFrequentlyRequestedUrl}\nCount: ${count}")
+            // Report on the parsing results...
+
+            // Most frequently requested URL overall
+            var mostFrequentlyRequestedUrl: Pair<String, Long>?
+            mostFrequentlyRequestedUrl = logProcessor.getMostFrequentlyRequestedUrl()
+            println("Most frequently requested URL:\nURL: ${mostFrequentlyRequestedUrl.first}\nCount: ${mostFrequentlyRequestedUrl.second}")
+
+            print("\n")
+
+            // Most frequently requested URL by specific date
+            val lookupDate = "1995-07-01"
+            mostFrequentlyRequestedUrl = logProcessor.getMostFrequentlyRequestedUrlForDate(lookupDate)
+            if (mostFrequentlyRequestedUrl != null) {
+                println("Most frequently requested URL on ${lookupDate}:\nURL: ${mostFrequentlyRequestedUrl.first}\nCount: ${mostFrequentlyRequestedUrl.second}")
+            } else {
+                println("No log entries exist for date: ${lookupDate}")
+            }
+
+            print("\n")
+
+            // Most frequently requested URL by date
+            val mostFrequentlyRequestedUrlsByDate = logProcessor.getMostFrequentlyRequestedUrlForEachDate()
+            println("Most frequently requested URLs by each date")
+            println("Date        URL (request count)")
+            println("----------  -------------------")
+            mostFrequentlyRequestedUrlsByDate.forEach {
+                println("${logProcessor.logDateFormat.format(it.first)}  ${it.second.key} (${it.second.value})")
+            }
+
         } else {
             println("ERROR: ${args[0]} does not exist")
             exitProcess(1)
@@ -42,8 +71,8 @@ fun main(args: Array<String>) {
 class LogProcessor {
     // Regex to get date and URL: group 1 = date, group 2 = URL
     private val dateAndUrlRegex = "\\[(.*)T.*\"[A-Z]+\\s([^\\s]+)\\s".toRegex()
-    private val logDateFormat = SimpleDateFormat("yyyy-MM-dd")
-    
+    var logDateFormat = SimpleDateFormat("yyyy-MM-dd")
+
     private val urlToCountMap = HashMap<String, Long>()
     private val dateToOccurrencesMap = HashMap<Date, HashMap<String, Long>>()
 
@@ -88,6 +117,37 @@ class LogProcessor {
         // Kotlin helper that finds map entry with maximum value
         val maxCountEntry = urlToCountMap.maxBy { it.value }
         return Pair(maxCountEntry!!.key, maxCountEntry.value)
+    }
+
+    /**
+     * Get the most frequently requested URL for a specific date
+     * @input logDate String using format: YYYY-MM-DD
+     * @return Pair containing the URL and its request count or null
+     *         if no entries exist for that date
+     */
+    fun getMostFrequentlyRequestedUrlForDate(logDate: String): Pair<String, Long>? {
+        val entriesDate = logDateFormat.parse(logDate)
+        var maxCountEntry: Map.Entry<String, Long>? = null
+        return if (dateToOccurrencesMap.containsKey(entriesDate)) {
+            val occurrencesMap = dateToOccurrencesMap[entriesDate]!!
+            maxCountEntry = occurrencesMap.maxBy { it.value }!!
+            Pair(maxCountEntry.key, maxCountEntry.value)
+        } else {
+            null
+        }
+    }
+
+    /**
+     * Return list of most requently access URLs by date, sorted by date descending
+     */
+    fun getMostFrequentlyRequestedUrlForEachDate(): List<Pair<Date, Map.Entry<String, Long>>> {
+        val mostFrequentUrlsByDate = dateToOccurrencesMap
+            .map { occurrencesMap ->
+                val occurrencesDate = occurrencesMap.key
+                val maxCountEntry = occurrencesMap.value.maxBy { it.value }!!
+                Pair(occurrencesDate, maxCountEntry)
+            }
+        return mostFrequentUrlsByDate.sortedWith(compareBy({it.first}))
     }
 
     /**
